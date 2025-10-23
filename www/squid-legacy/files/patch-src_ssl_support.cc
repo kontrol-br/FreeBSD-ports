3c161fb66e79d773a038f4fb76d014c294012ea5
KONTROL Tecnologia <guzzy@uol.com.br>
2025-10-23 20:51:42 -0300

    www/squid-legacy: fix ssl support patch format

--- www/squid-legacy/files/patch-src_ssl_support.cc
+++ www/squid-legacy/files/patch-src_ssl_support.cc
@@ -1,17 +1,16 @@
 --- src/ssl/support.cc.orig	2021-05-30 20:00:00 UTC
 +++ src/ssl/support.cc
-@@
+@@ -1 +1,5 @@
 -    ssl_ex_index_cert_error_check = SSL_get_ex_new_index(0, (void *) "cert_error_check", NULL, &ssl_dupAclChecklist, &ssl_freeAclChecklist);
 +#if OPENSSL_VERSION_NUMBER >= 0x30000000L
 +    ssl_ex_index_cert_error_check = SSL_get_ex_new_index(0, (void *) "cert_error_check", NULL, &ssl_dupAclChecklist_ossl3, &ssl_freeAclChecklist);
 +#else
 +    ssl_ex_index_cert_error_check = SSL_get_ex_new_index(0, (void *) "cert_error_check", NULL, &ssl_dupAclChecklist, &ssl_freeAclChecklist);
 +#endif
-@@
+@@ -1,3 +1,12 @@
      CRYPTO_set_ex_data(to, idx, dupChecklist);
      return 1;
  }
-
 +#if OPENSSL_VERSION_NUMBER >= 0x30000000L
 +static int
 +ssl_dupAclChecklist_ossl3(CRYPTO_EX_DATA *to, const CRYPTO_EX_DATA *from,
